7760758d29d7227f346f1eec909c25cc1a72458c
varun <varunswarup0@gmail.com>
2020-04-19 21:33:55 +0530

    event arg

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const EventEmitter = require("events");
 const emitter = new EventEmitter();
 
-//Register a lsitener
-emitter.on("messageLogged", function () {
-  console.log("Listener Called");
+//Register a listener
+emitter.on("messageLogged", (arg) => {
+  console.log("Listener Called", arg);
 });
 
 // Raise an Event
-emitter.emit("messageLogged");
+emitter.emit("messageLogged", { id: 1, url: "http://" });
+
+emitter.on("logging", (arg) => {
+  console.log("Listener Called", arg);
+});
+emitter.emit("logging", { data: "message" });

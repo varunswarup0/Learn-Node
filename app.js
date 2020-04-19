b57c151c7dba60684744f3510e7d93c8309f9e45
varun <varunswarup0@gmail.com>
2020-04-19 22:34:04 +0530

    http module

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-const EventEmitter = require("events");
+const http = require("http");
 
-const Logger = require("./logger");
-const logger = new Logger();
-
-//Register a listener
-logger.on("messageLogged", (arg) => {
-  console.log("Listener Called", arg);
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.write("Hello World");
+    res.end();
+  }
+  if (req.url === "/api/courses") {
+    res.write(JSON.stringify([1, 2, 3]));
+    res.end();
+  }
 });
 
-logger.log("message");
+server.listen(3000);
+
+console.log("Listening on port 3000 ...");

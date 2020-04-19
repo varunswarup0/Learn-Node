ab727ea56f4f785a37de2b726a572252d1267838
varun <varunswarup0@gmail.com>
2020-04-19 22:17:41 +0530

    Extending Event Emiiter

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
 const EventEmitter = require("events");
-const emitter = new EventEmitter();
+
+const Logger = require("./logger");
+const logger = new Logger();
 
 //Register a listener
-emitter.on("messageLogged", (arg) => {
+logger.on("messageLogged", (arg) => {
   console.log("Listener Called", arg);
 });
 
-// Raise an Event
-emitter.emit("messageLogged", { id: 1, url: "http://" });
-
-emitter.on("logging", (arg) => {
-  console.log("Listener Called", arg);
-});
-emitter.emit("logging", { data: "message" });
+logger.log("message");
